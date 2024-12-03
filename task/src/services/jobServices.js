650e5f5a1ef76165860fd091b60723ca901bb688
Grave994 <heroesqvantanamela.123@gmail.com>
2024-12-03 08:28:23 +0400

    vacancies

---
+++ task/src/services/jobServices.js
@@ -0,0 +1,15 @@
+export async function fetchData() {
+    const endpoint = 'vacancies';
+    try {
+        const response = await fetch("https://flower-honeysuckle-empress.glitch.me/vacancies");
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+
+    } catch (err) {
+        console.error("Error while fetching data:", err);
+        return [];
+    }
+}
